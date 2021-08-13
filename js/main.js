ae84f85c290cd8ab3202881fa4b317733435e4a2
Ana Clara <ana.claraop@outlook.com>
2021-08-13 01:44:01 -0300

    aula_javascript

---
+++ js/main.js
@@ -0,0 +1,119 @@
+
+function clicou(){
+    document.getElementById("Agradecimento").innerHTML = "<b>Obrigada por clicar</b>";
+}
+
+function redirecionar (){
+    window.open("https://br.pinterest.com/");
+    //window.location.href = "https://br.pinterest.com/";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!";
+    //alert("Trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui.";
+}
+
+
+//console.log(document.getElementById("Agradecimento"));
+//}
+
+//alert("Obrigado por clicar!");
+//}
+
+//function soma (n1,n2){
+//    return n1 + n2;
+//}
+
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+
+/*
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão!", "Japão", "Brasil"));
+*/
+
+/*
+var d = new Date();
+alert(d.getDate());
+*/
+
+/*
+var count;
+for (count=0; count <= 5; count ++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+};
+*/
+/*
+var idade = prompt ("Qual é a sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Ana Clara";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo."
+//alert(idade + idade2)
+//alert (nome + " tem " + idade + " anos.");
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
